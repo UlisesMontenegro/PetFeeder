525eaa952718717b6988e2e4e828ffbe698dd147
UlisesMontenegro <umontenegro@fi.uba.ar>
2023-06-12 17:59:48 +0000

    First version

---
+++ main.cpp
@@ -0,0 +1,29 @@
+/*! @mainpage PetFeeder
+ * @date Thursday, March 16, 2023
+ * @authors Ulises Montenegro
+ *
+ * @note First version of the main behaviour of the project.
+ * When the potentiometer (acts as a weight sensor) reachs a defined value, the LED turns on.
+ * When the button is pressed, the LED turns off.
+ * Potentiometer: PINX
+ * Led: 
+ * Button: 
+ */
+
+
+//=====[Libraries]=============================================================
+
+#include "mbed.h"
+#include "pet_feeder.h"
+
+
+//=====[Main function, the program entry point after power on or reset]========
+
+int main()
+{
+    petFeederInit();
+    while (true) {
+        petFeederUpdate();
+    }
+}
+
---
+++ pet_feeder.cpp
@@ -0,0 +1,33 @@
+//=====[Libraries]=============================================================
+
+#include "pet_feeder.h"
+#include "arm_book_lib.h"
+
+//=====[Declaration of private defines]========================================
+
+//=====[Declaration of private data types]=====================================
+
+//=====[Declaration and initialization of public global objects]===============
+
+//=====[Declaration of external public global variables]=======================
+
+//=====[Declaration and initialization of public global variables]=============
+
+//=====[Declaration and initialization of private global variables]============
+
+//=====[Declarations (prototypes) of private functions]========================
+
+//=====[Implementations of public functions]===================================
+
+void petFeederInit()
+{
+    //Initialize each module.
+}
+
+void petFeederUpdate()
+{
+    //Update each module.
+    delay(SYSTEM_TIME_INCREMENT_MS);
+}
+
+//=====[Implementations of private functions]==================================
